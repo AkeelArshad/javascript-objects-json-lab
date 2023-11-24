fbcc17869a0e03602b393d51ba4bc80fe98b6e48
Arshad <c2007010@hallam.shu.ac.uk>
2023-11-24 11:45:24 +0000

    Added Javascript code to identify where the new element is.

--- js/clock.js
+++ js/clock.js
@@ -4,6 +4,7 @@
     console.info("Hello JS");
 
 const myNode = document.createElement("div");
+const digitalClock = document.getElementById("digitalClock");
 digitalClock.appendChild(myNode);
 
 })();
