9ed4458cd6cd2b2f9b8e55ed84b7b3a10568c255
Arshad <c2007010@hallam.shu.ac.uk>
2023-11-24 12:21:55 +0000

    Moved Javascript code

--- js/clock.js
+++ js/clock.js
@@ -11,14 +11,13 @@ const myDate = new Date();
 console.dir(myDate);
 
 // show only hours, minutes and seconds
-myNode.innerHTML = myDate.toLocaleTimeString([], {hour: "2-digit", minute: "2-digit", second: "2-digit"});
 
 
 const updateTime = function()
 {
     console.info('tick tock');
+    myNode.innerHTML = myDate.toLocaleTimeString([], {hour: "2-digit", minute: "2-digit", second: "2-digit"});
 }
-
 setInterval(updateTime, 1000);
 
 })();
