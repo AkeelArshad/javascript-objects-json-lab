407a59dcf0b70f669acdf6a581afbf83ef65ec01
Arshad <c2007010@hallam.shu.ac.uk>
2023-11-24 11:53:54 +0000

    Added Javascript code to add some content in and added Javascript code to create a new Date object.

--- js/clock.js
+++ js/clock.js
@@ -6,5 +6,8 @@
 const myNode = document.createElement("div");
 const digitalClock = document.getElementById("digitalClock");
 digitalClock.appendChild(myNode);
+myNode.innerHTML = "Welcome";
+
+const myDate = new Date();
 
 })();
