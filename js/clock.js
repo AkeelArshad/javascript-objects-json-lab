adcf0680526f47232745b057c0c0b3fa7e45981f
Arshad <c2007010@hallam.shu.ac.uk>
2023-11-24 11:58:42 +0000

    Added Javascript to add the current date to the DOM.

--- js/clock.js
+++ js/clock.js
@@ -6,9 +6,9 @@
 const myNode = document.createElement("div");
 const digitalClock = document.getElementById("digitalClock");
 digitalClock.appendChild(myNode);
-myNode.innerHTML = "Welcome";
 
 const myDate = new Date();
 console.dir(myDate);
+myNode.innerHTML = myDate.toDateString();
 
 })();
